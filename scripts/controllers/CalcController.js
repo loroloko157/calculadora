e7d43219ddc4d08ecbc8ecae054c2a4a38def5e9
wanderson <wanderson_c_c@hotmail.com>
2020-01-29 20:27:08 -0300

    added = function

--- scripts/controllers/CalcController.js
+++ scripts/controllers/CalcController.js
@@ -3,6 +3,9 @@ class CalcController {
     constructor() {
         //atributos
 
+        this._lastOperator ='';
+        this._lastNumber = '';
+
         this._operation = [];
         this._locale = "pt-BR"
         this._displayCalcEl = document.querySelector("#display");
@@ -83,16 +86,35 @@ class CalcController {
 
     }
 
+    getResult(){
+
+        return eval(this._operation.join(""))
+
+    }
+
+
     calc(){
 
         let last = '';
+        this._lastOperator = this.getLastItem();
 
-        if(this._operation.length > 3){
-            last = this._operation.pop();
+        if(this._operation.length < 3 ) {
+
+            let firstItem = this._operation[0];
+            this._operation = [firstItem, this._lastOperator, this._lastNumber];
         }
 
-         
-        let result = eval(this._operation.join(""))
+        if(this._operation.length > 3){
+
+            last = this._operation.pop();
+            this._lastNumber = this.getResult();
+        }else if(this._operation.length == 3) {
+
+            this._lastNumber = this.getLastItem(false);
+        }
+
+       
+        let result =  this.getResult()
 
         if(last == '%'){
 
@@ -112,21 +134,33 @@ class CalcController {
         this.setLastNumberToDisplay()
     }
 
+
+    getLastItem(isOperator = true){
+        let lastItem;
+
+        for(let i = this._operation.length - 1; i >= 0; i--){
+
+            if(this.isOperator(this._operation[i]) == isOperator){
+                lastItem = this._operation[i];
+                break;
+            }
+
+        }
+        if(!lastItem) {
+
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber
+        }
+
+        return lastItem;
+    }
+
     setLastNumberToDisplay(){
 
-        let lastNumber;
+        let lastNumber = this.getLastItem(false)
 
-                    for(let i = this._operation.length-1; i >= 0; i--){
 
-                        if(!this.isOperator(this._operation[i])){
-                            lastNumber = this._operation[i]
-                            break
-                        }
-
-                    }
-
-                    if(!lastNumber) lastNumber = 0
-                    this.displayCalc = lastNumber
+            if(!lastNumber) lastNumber = 0
+            this.displayCalc = lastNumber
 
     }
 
