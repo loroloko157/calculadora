5d91dac7c466ab8b0cf0d8a9152d5cb58ac96a08
wanderson <wanderson_c_c@hotmail.com>
2020-02-02 10:12:43 -0300

    new added update

--- scripts/controllers/CalcController.js
+++ scripts/controllers/CalcController.js
@@ -197,7 +197,7 @@ class CalcController {
             } else {
 
                 let newValue = this.getLastOperation().toString() + value.toString()
-                this.setLastOperation(parseFloat(newValue));
+                this.setLastOperation(newValue);
 
                 this.setLastNumberToDisplay()
 
@@ -220,6 +220,7 @@ class CalcController {
 
         let lastOperation = this.getLastOperation()
 
+        if(typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > - 1) return;
         if(this.isOperator(lastOperation) || !lastOperation){
             this.pushOperation('0.');
         } else {
